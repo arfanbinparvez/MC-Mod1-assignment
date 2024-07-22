e96f76e78fe60790957eaa09ebee11ba4e7c997e
arfanbinparvez <123824765+arfanbinparvez@users.noreply.github.com>
2024-07-22 20:44:07 +0530

    Update mod1_assignement.sol

--- mod1_assignement.sol
+++ mod1_assignement.sol
@@ -1,38 +1,23 @@
-
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
 contract ErrorHandlingExample {
     uint256 public value;
 
-    // Function to set a value with require statement
     function setValueWithRequire(uint256 _value) public {
-        require(_value > 0, "Value must be greater than 0");
+        require(_value > 10, "Value must be greater than 10");
         value = _value;
     }
 
-    // Function to set a value with assert statement
     function setValueWithAssert(uint256 _value) public {
-        value = _value;
-        // This assert statement should never fail if the contract works correctly
-        assert(value == _value);
+        value = _value * 2; // intentionally introduced an error
+        assert(value == _value); // this assert should fail
     }
 
-    // Function to demonstrate revert statement
     function setValueWithRevert(uint256 _value) public {
-        if (_value == 0) {
-            revert("Value cannot be 0");
+        if (_value < 5) {
+            revert("Value must be 5 or greater");
         }
         value = _value;
-    }
-
-    // Function to demonstrate all three statements
-    function setValueWithAll(uint256 _value) public {
-        require(_value > 0, "Value must be greater than 0");
-        value = _value;
-        assert(value == _value);
-        if (_value == 1) {
-            revert("Value cannot be 1");
-        }
     }
 }
